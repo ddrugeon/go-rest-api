89aa57816bc9954ad940e24f6b52935a0efd85d2
David Drugeon-Hamon <zebeurton@gmail.com>
2020-06-22 22:59:52 +0200

    Add healthz route to give current status of application

--- cmd/droid/main.go
+++ cmd/droid/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+var status HealthStatus
+
+// HealthStatus defines status of current service
+type HealthStatus struct {
+	Status string `json:"status"`
+}
+
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	status.Status = "OK"
+	json.NewEncoder(w).Encode(status)
+}
 
 func handle(w http.ResponseWriter, r *http.Request) {
 	if "/favicon.ico" == r.URL.Path {
@@ -23,6 +37,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/droids", handle).Methods("GET")
+	router.HandleFunc("/healthz", handleHealthz).Methods("GET")
 
 	http.ListenAndServe(":8000", router)
 }
