a0969df87d4f0e3f5d425adb625b4174253cf516
David Drugeon-Hamon <zebeurton@gmail.com>
2020-06-25 12:45:13 +0200

    Add logrus logger

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -11,18 +10,24 @@ import (
 	"github.com/ddrugeon/go-rest-api/internal/middlewares"
 	"github.com/ddrugeon/go-rest-api/internal/router"
 	"github.com/gorilla/mux"
+	negronilogus "github.com/meatballhat/negroni-logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/negroni"
 )
 
 type Server struct {
 	router *mux.Router
 	server http.Server
+	port   string
+	logger *logrus.Logger
 }
 
 func NewServer(app *app.App) Server {
 	router.InitRoutes(app)
 	return Server{
 		router: router.NewRouter(app),
+		port:   app.Port,
+		logger: app.Logger,
 	}
 }
 
@@ -31,19 +36,20 @@ func (s *Server) Initialize() {
 }
 
 // Run application server
-func (s *Server) Run(addr string) {
+func (s *Server) Run() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL, os.Interrupt, os.Kill)
 
 	go func() {
-		n := negroni.Classic()
+		n := negroni.New()
+		n.Use(negronilogus.NewMiddlewareFromLogger(s.logger, "web"))
 		n.Use(middlewares.NewJSONContentTypeMiddleware())
 		n.UseHandler(s.router)
-		log.Fatal(http.ListenAndServe(addr, n))
+		s.logger.Fatal(http.ListenAndServe(s.port, n))
 
 	}()
 
-	log.Printf("Server is listning on http://%s\n", addr)
+	s.logger.Printf("Server is listening on http://%s\n", s.port)
 	sig := <-sigs
-	log.Println("Signal: ", sig)
+	s.logger.Println("Signal: ", sig)
 }
