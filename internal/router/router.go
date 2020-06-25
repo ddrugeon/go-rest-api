b3e6621525622df3da56b16ea49e29cd6d34e262
David Drugeon-Hamon <zebeurton@gmail.com>
2020-06-25 10:10:50 +0200

    refactoring: create new package middlewares

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,15 +3,16 @@ package router
 import (
 	"github.com/ddrugeon/go-rest-api/internal/app"
 	"github.com/ddrugeon/go-rest-api/internal/handlers"
+	"github.com/ddrugeon/go-rest-api/internal/middlewares"
 	"github.com/gorilla/mux"
 )
 
 func NewRouter(app *app.App) *mux.Router {
 	router := mux.NewRouter()
-	router.Use(handlers.JSONContentTypeMiddleware)
+	router.Use(middlewares.JSONContentTypeMiddleware)
 	router.Handle("/healthz", handlers.Health(app)).Methods("GET")
 	router.Handle("/droids", handlers.GetAllDroids(app)).Methods("GET")
-	router.Handle("/droid/{id}", handlers.GetDroidByID(app)).Methods("GET")
+	router.Handle("/droids/{id}", handlers.GetDroidByID(app)).Methods("GET")
 
 	return router
 }
